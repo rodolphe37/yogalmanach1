8e8f2d2ed051af92066506d12f10ea7d95beadfb
rodolphe37 <rodolphe.a@gmail.com>
2021-03-13 00:20:15 +0100

    first commit

--- components/CarItem/styles.js
+++ components/CarItem/styles.js
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
   },
 
   image: {
-    width: "100%",
+    width: Dimensions.get("window").width,
     height: "100%",
     resizeMode: "cover",
     position: "absolute",
